16632df24b2145d918a93c16a919578340063ff0
ForgottenBeast <quentin.mallet@gmail.com>
2018-10-08 08:53:22 -0400

    DEBUG data agent ps1 print

--- data/agent/agent.ps1
+++ data/agent/agent.ps1
@@ -778,7 +778,7 @@ function Invoke-Empire {
 	$script:SendMessage = {
 		param($Listener, $Packets)
         "called send message" | Out-File "out.log" -Append -NoClobber
-        $PacketData | Out-File "out.log" -Append -NoClobber
+        $Packets | Out-File "out.log" -Append -NoClobber
         (& $Listener["send_func"] -Packets $Packets -FixedParameters $Listener["fixed_parameters"])
 	}
 
@@ -791,6 +791,7 @@ function Invoke-Empire {
 		}
 		else {
 			if ([System.Text.Encoding]::UTF8.GetString($TaskData) -ne $Listener['defaultResponse']) {
+                "got something not equal to defaultResponse, calling decoderoutingpacket"
 				Decode-RoutingPacket -PacketData $TaskData
 			}
 		}
