4ae081de547d85fb6138cb05b58e35eebc25cc79
ForgottenBeast <quentin.mallet@gmail.com>
2018-10-11 07:14:37 -0400

    lib listeners http hop py fixes

--- lib/listeners/http_hop.py
+++ lib/listeners/http_hop.py
@@ -522,7 +522,7 @@ class Listener:
     'delay' : {delay},
     'jitter' : {jitter},
     'fixed_parameters': {{
-        'headers' : {{'User-Agent': "{UA}", 'Cookie':"{cookie}"}},
+        'headers' : {{'User-Agent': "{UA}" }},
         'taskURIs' : "{taskURIs}",
     }},
     'send_func': {send_func},
@@ -544,7 +544,6 @@ def send_message_{name}(packets=None,**kwargs):
     taskURIs = kwargs['taskURIs'].split(',')
 
     {update_servers}
-    {https}
 
     data = None
     if packets:
@@ -577,7 +576,17 @@ def send_message_{name}(packets=None,**kwargs):
     return ('', '')
 #COMM_FUNCTION
 """
-                return updateServers + sendMessage
+                return( listener_dict.format(
+                            send_func = "send_message_{}".format(listenerOptions['Name']['Value']),
+                            delay = delay,
+                            jitter = jitter,
+                            UA = profile.split('|')[1],
+                            name = listenerOptions['Name']['Value'],
+                            taskURIs = profile.split('|')[0],
+                            lostLimit = lostLimit,
+                            defaultResponse = b64DefaultResponse),
+                            sendMessage.format(name=listenerOptions['Name']['Value'],
+                            update_servers = updateServers))
 
             else:
                 print helpers.color("[!] listeners/http_hop generate_comms(): invalid language specification, only 'powershell' and 'python' are current supported for this module.")
